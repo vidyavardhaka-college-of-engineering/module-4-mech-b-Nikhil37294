e2fd9165ec3e4745addb9fc667d51f574edaf25d
Nikhil37294 <20me0125@vvce.ac.in>
2021-03-30 14:09:27 +0000

    w

--- Program-4.c
+++ Program-4.c
@@ -18,6 +18,5 @@ int main()
     printf("Enter a number1,number2,number3:\n");
     scanf("%f%f%f", &enternum.num1,&enternum.num2,&enternum.num3);
      //compute and print the average
-     avg.average=(enternum.num1+enternum.num2)
     return 0;
-} 
+}
